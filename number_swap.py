d844f2cbd17b39674d7c17388539789ba2e36366
atharv173 <varshakadam156@gmail.com>
2026-04-22 08:32:50 -0700

    Add files via upload

---
+++ number_swap.py
@@ -0,0 +1,9 @@
+x=input("enter a value: ")
+z=input("enter a value: ")
+y=input("enter a value: ")
+print(x,y,z)
+a=x
+x=z
+z=y
+y=a
+print(x,y,z)
---
+++ power_calculator.py
@@ -0,0 +1,3 @@
+x=int(input("enter a base number: "))
+z=int(input("enter a power number: "))
+print(x**z)
