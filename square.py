0b3de510b23cffd22d93e858ed046eec6fa66912
atharv173 <varshakadam156@gmail.com>
2026-05-02 01:46:01 -0700

    Add files via upload

---
+++ square.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(300,400)
+square=turtle.Turtle()
+num_sides = 4
+side_length = 50
+angle = 360.0 / num_sides
+
+for i in range (num_sides):
+    square.forward(side_length)
+    square.right(angle)
+turtle.done()
